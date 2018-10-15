49df86fbc03b6102ec424d51a71e2c0b6f567aff
kushagramishra00 <44142171+kushagramishra00@users.noreply.github.com>
2018-10-15 19:15:43 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class a
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       for(int i=0;i<n;i++)
+       {
+           System.out.println("Hello");
+       }
+    }
+}
