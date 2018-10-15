1827126ecd7ffae262decb60618190fcdb327c57
kushagramishra00 <44142171+kushagramishra00@users.noreply.github.com>
2018-10-15 13:17:03 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class a 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>0)
+        {
+        if(n%2==0)
+        {
+            System.out.println("Even");
+            }
+        else
+        System.out.println("Odd");
+       
+        }
+        else
+         System.out.println("Invalid");
+}
+}
