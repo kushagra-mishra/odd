8bb757412bf391402a1228985d8cfde9c79eae66
kushagramishra00 <44142171+kushagramishra00@users.noreply.github.com>
2018-10-15 18:38:52 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class a
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc=new Scanner(System.in);
+        char c=(char) System.in.read();
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+            System.out.println("Vowel");
+        else
+        System.out.println("Constonant");
+        }
+        }
+            
