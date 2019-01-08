75606d27123778f0d78f23601d7e69a8e3cf5ba2
kushagra-mishra <35230766+kushagra-mishra@users.noreply.github.com>
2019-01-08 21:36:23 +0530

    Update and rename README.md to power.java

---
+++ power.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class pow
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        double a=Math.pow(n,k);
+        System.out.println(a);
+    }
+}
