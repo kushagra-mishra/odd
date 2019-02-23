07467b887b8f541279a29ac27ee90d99fe7ac968
kushagra-mishra <35230766+kushagra-mishra@users.noreply.github.com>
2019-02-23 22:02:50 +0530

    Rename reverse.java to reverse2.java

---
+++ reverse2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.math.BigInteger;
+class fact
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=0;
+        while(n!=0)
+        {
+            int d=n%10;
+            s=s*10+d;
+            n=n/10;
+        }
+        System.out.println(s);
+}
+}
