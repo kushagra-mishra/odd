4ce919324d8f7613bff9780ea8c446e1555ccb43
kushagra-mishra <35230766+kushagra-mishra@users.noreply.github.com>
2019-02-23 21:31:29 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+class Number{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        StringBuilder input1=new StringBuilder();
+        input1.append(s);
+        input1=input1.reverse();
+        System.out.println(input1);
+    }
+  
+}
